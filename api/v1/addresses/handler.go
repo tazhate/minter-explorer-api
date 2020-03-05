81e3d93fc7096bf5be8d0081bb7f0eb930a480f5
Kamil Mukhametzyanov <grkamil@yandex.ru>
2020-03-05 18:26:04 +0300

    temp fix: tools v4

--- api/v1/addresses/handler.go
+++ api/v1/addresses/handler.go
@@ -15,7 +15,7 @@ import (
 	"github.com/MinterTeam/minter-explorer-api/slash"
 	"github.com/MinterTeam/minter-explorer-api/tools"
 	"github.com/MinterTeam/minter-explorer-api/transaction"
-	"github.com/MinterTeam/minter-explorer-tools/models"
+	"github.com/MinterTeam/minter-explorer-tools/v4/models"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
--- transaction/data_resources/redeem_check.go
+++ transaction/data_resources/redeem_check.go
@@ -4,7 +4,7 @@ import (
 	"encoding/base64"
 	"github.com/MinterTeam/minter-explorer-api/helpers"
 	"github.com/MinterTeam/minter-explorer-api/resource"
-	"github.com/MinterTeam/minter-explorer-tools/models"
+	"github.com/MinterTeam/minter-explorer-tools/v4/models"
 	"github.com/MinterTeam/minter-go-node/core/check"
 )
 
